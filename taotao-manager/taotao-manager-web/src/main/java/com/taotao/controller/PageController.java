126a321f58f45a0359d5b4a79afb97c80129a926
miss137 <1573943218@qq.com>
2020-04-09 19:31:50 +0800

    测试分页插件以及pom文件分页依赖版本修改

--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,6 +14,17 @@ public class PageController {
      */
     @RequestMapping("/")
     public  String showIndex(){
+
         return  "index";
+    }
+
+    /**
+     * 展示其他页面
+     * @param page
+     * @return
+     */
+    @RequestMapping("/{page}")
+    public String showpage(@PathVariable String  page){
+        return page;
     }
 }
